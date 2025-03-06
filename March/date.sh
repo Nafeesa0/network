249f042b3cf883340325b015cc39de561a234732
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-06 11:15:23 +0530

    Update date.sh

--- March/date.sh
+++ March/date.sh
@@ -11,3 +11,13 @@ echo "Current Date and Time : $current_date_time"
 echo "Username : $username"
 
 echo "Current working Directory : $current_dir"
+
+
+OUTPUT :
+
+[mca@localhost s2mca]$ vi date.sh
+[mca@localhost s2mca]$ chmod +x date.sh
+[mca@localhost s2mca]$ ./date.sh
+Current Date and Time : Thu Mar  6 10:41:53 AM IST 2025
+Username : mca
+Current working Directory : /home/mca/s2mca
