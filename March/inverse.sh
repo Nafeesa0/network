128bb0b109408733db5003e5d37bb0ad49ae9bfb
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-06 11:19:32 +0530

    Update inverse.sh

--- March/inverse.sh
+++ March/inverse.sh
@@ -9,3 +9,24 @@ do
 	echo "$i"
 done
 
+
+
+OUTPUT :
+
+
+
+[mca@localhost s2mca]$ vi inverse.sh
+[mca@localhost s2mca]$ chmod +x inverse.sh
+[mca@localhost s2mca]$ ./inverse.sh
+Enter the limit : 
+10
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
