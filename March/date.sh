20efb48515fbd4217463e30ed11c9b37d9b61a80
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-06 11:05:12 +0530

    Add files via upload

---
+++ March/date.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+current_date_time=$(date)
+
+username=$(whoami)
+
+current_dir=$(pwd)
+
+echo "Current Date and Time : $current_date_time"
+
+echo "Username : $username"
+
+echo "Current working Directory : $current_dir"
