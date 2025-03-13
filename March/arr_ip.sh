a026240bc12da6464790459536ce5b873b0c916f
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-13 11:26:41 +0530

    Update arr_ip.sh

--- March/arr_ip.sh
+++ March/arr_ip.sh
@@ -13,3 +13,20 @@ for element in "${arr[@]}"; do
     echo "$element"
 done
 
+OUTPUT :
+
+[mca@localhost s2mca]$ vi arr_ip.sh
+[mca@localhost s2mca]$ chmod +x arr_ip.sh
+[mca@localhost s2mca]$ ./arr_ip.sh
+Enter the number of elements:
+4
+Enter the elements:
+america 
+2 
+3
+4
+The elements in the array are:
+america
+2
+3
+4
