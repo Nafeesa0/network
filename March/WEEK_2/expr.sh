942d3d4e780596419327562e3d7bc3a746e5ec57
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-13 11:34:07 +0530

    Update expr.sh

--- March/WEEK_2/expr.sh
+++ March/WEEK_2/expr.sh
@@ -17,4 +17,21 @@ result=$((a * 20 - b * 2 + c / d))
 echo "The result of the expression (a*20 - b*2 + c/d) is: $result"
 
 
+OUTPUT :
+
+
+[mca@localhost s2mca]$ vi expr.sh
+[mca@localhost s2mca]$ chmod +x expr.sh
+[mca@localhost s2mca]$ ./expr.sh
+Enter value for a: 
+2
+Enter value for b: 
+4
+Enter value for c: 
+5
+Enter value for d: 
+7
+The result of the expression (a*20 - b*2 + c/d) is: 32
+
+
 
