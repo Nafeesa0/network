7a7ea6aae8bedefd81adf5edf6373e5d5be0264c
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-13 11:01:23 +0530

    Update palindrome.sh

--- March/palindrome.sh
+++ March/palindrome.sh
@@ -10,3 +10,18 @@ else
     echo "The string is not a palindrome."
 fi
 
+
+
+OUTPUT :
+
+[mca@localhost s2mca]$ vi palindrome.sh
+[mca@localhost s2mca]$ chmod +x palindrome.sh
+[mca@localhost s2mca]$ ./palindrome.sh
+Enter a string : 
+malayalam
+The string is a palindrome.
+
+[mca@localhost s2mca]$ ./palindrome.sh
+Enter a string : 
+america
+The string is not a palindrome.
