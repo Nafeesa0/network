988d4a21eb799bdba2ec864806836f0f84030b51
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-13 10:59:05 +0530

    Add files via upload

---
+++ March/palindrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter a string : "
+read s
+
+if [[ "$s" == "$(echo "$s" | rev)" ]];
+then
+    echo "The string is a palindrome."
+else
+    echo "The string is not a palindrome."
+fi
+
