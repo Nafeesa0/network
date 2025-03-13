4df9665a13120c218a27ee953c748cdd17b5c6b6
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-13 11:22:54 +0530

    Update pswrd.sh

--- March/pswrd.sh
+++ March/pswrd.sh
@@ -44,3 +44,19 @@ echo "Enter a password: "
 read -s password
 
 validate_password "$password"
+
+
+OUTPUT :
+
+[mca@localhost s2mca]$ vi pswrd.sh
+[mca@localhost s2mca]$ chmod +x pswrd.sh
+[mca@localhost s2mca]$ ./pswrd.sh
+Enter a password: 
+ Admin123 
+Password must contain at least one special character.
+[mca@localhost s2mca]$ vi pswrd.sh
+[mca@localhost s2mca]$ chmod +x pswrd.sh
+[mca@localhost s2mca]$ ./pswrd.sh
+Enter a password: 
+ Admin@123 
+Password is strong.
