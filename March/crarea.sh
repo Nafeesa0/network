ea963cb47ee55a22c9a6e18c89108e898ce762bc
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-13 10:57:39 +0530

    Add files via upload

---
+++ March/crarea.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "enter the radius of the circle : "
+
+read r
+
+pi=3.14
+
+area=$(echo "$pi * $r * $r" | bc)
+calculate_area() {
+    local radius=$1
+    local area=$(echo "$pi * $radius * $radius" | bc)
+    echo "The area of the circle  is  $area"
+}
+
+
+calculate_area $r
