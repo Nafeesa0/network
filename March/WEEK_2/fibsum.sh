5ab3ae0c39abaec5e8fc8161e0508268a83b0652
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-14 10:35:01 +0530

    Update fibsum.sh

--- March/WEEK_2/fibsum.sh
+++ March/WEEK_2/fibsum.sh
@@ -17,3 +17,10 @@ done
 echo
 echo "Sum of the Fibonacci series up to 10 terms: $sum"
 
+OUTPUT :
+
+[mca@localhost s2mca]$ vi fibsum.sh
+[mca@localhost s2mca]$ chmod +x fibsum.sh
+[mca@localhost s2mca]$ ./fibsum.sh
+0 1 1 2 3 5 8 13 21 34 
+Sum of the Fibonacci series up to 10 terms: 88
